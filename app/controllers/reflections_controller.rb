0b9aa55676ad2075d79c4a1cd6cfc57d5afaf945
Ildar Safin <safild94@gmail.com>
2018-03-07 14:16:19 +0100

    Change ordering by creation date in reflection controller

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,5 +1,5 @@
 class ReflectionsController < ApplicationController
   def index
-    @reflections = current_user.reflections
+    @reflections = current_user.reflections.order(created_at: :desc)
   end
 end
