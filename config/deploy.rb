084ad2e806ddb5e87e784229f20ff1d308dec148
Ildar Safin <safild94@gmail.com>
2018-06-09 17:16:48 +0200

    Change version of Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock "~> 3.11.0"
 
 set :application, "reflections"
 set :repo_url, "https://github.com/ildarsafin/myreflections"
