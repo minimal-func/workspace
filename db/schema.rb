a023035a3959268062c478ecf7339048efb42f69
Ildar <knock@ildarsafin.tech>
2022-11-02 22:45:23 +0100

    Remove content from reflections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_151441) do
+ActiveRecord::Schema.define(version: 2022_11_02_214447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,7 +130,6 @@ ActiveRecord::Schema.define(version: 2020_12_26_151441) do
   end
 
   create_table "reflections", force: :cascade do |t|
-    t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
