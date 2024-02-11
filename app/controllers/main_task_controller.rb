a766c3003adfb86afd8f48f2f0a0a7b14f4f2516
Ildar Safin <knock@ildarsafin.tech>
2024-02-11 11:34:00 +0100

    FIx MainTask updating

--- app/controllers/main_task_controller.rb
+++ app/controllers/main_task_controller.rb
@@ -6,7 +6,7 @@ class MainTaskController < ApplicationController
   def create
     main_task = current_user.main_task
 
-    return redirect_to dashboards_url if main_task && main_task.update_attributes(main_task_params)
+    return redirect_to dashboards_url if main_task && main_task.update!(main_task_params)
     return redirect_to dashboards_url if !main_task && current_user.create_main_task(main_task_params)
 
     render :new
