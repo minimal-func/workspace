73f775ba820c3c336dbcd406dd2b0af004e957ca
Ildar Safin <knock@ildarsafin.tech>
2025-07-12 16:06:05 +0200

    Add paginatin

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,5 +1,5 @@
 class ReflectionsController < ApplicationController
   def index
-    @reflections = current_user.reflections.order(created_at: :desc).with_rich_text_content
+    @pagy, @reflections = pagy(current_user.reflections.order(created_at: :desc).with_rich_text_content, items: 10)
   end
 end
