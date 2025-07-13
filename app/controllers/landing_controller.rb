b0545e8eae0d96cd44d0be95596cd6eb04798960
Ildar Safin <knock@ildarsafin.tech>
2025-07-13 10:32:16 +0200

    Redirect to the dashboard if user is signed_in?

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,6 +2,10 @@ class LandingController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   
   def index
+    if user_signed_in?
+      redirect_to dashboards_path
+    end
+
     @app_title = 'Personal Productivity'
   end
 end
