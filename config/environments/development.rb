051137c867624eba9b0a34eab732868a6ea05103
Ildar Safin <knock@ildarsafin.tech>
2025-07-13 08:52:28 +0200

    Add functional header element Projects

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = false
+
+  config.after_initialize do
+    ActiveStorage::Current.url_options = {host: "localhost", port: 3000}
+  end
 end
