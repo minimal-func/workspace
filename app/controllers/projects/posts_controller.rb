84052b7d437b5a3410a5829d62c34d6bd4ae1913
Ildar Safin <knock@ildarsafin.tech>
2025-08-23 10:12:21 +0200

    Add public/private posts

--- app/controllers/projects/posts_controller.rb
+++ app/controllers/projects/posts_controller.rb
@@ -21,11 +21,23 @@ module Projects
     end
 
     def index
-      @posts = @project.posts.with_rich_text_content_and_embeds
+      if current_user && can_update_resource?(@project)
+        # Show all posts to project owner/collaborators
+        @posts = @project.posts.with_rich_text_content_and_embeds
+      else
+        # Show only public posts to others
+        @posts = @project.posts.where(public: true).with_rich_text_content_and_embeds
+      end
     end
 
     def show
       @project = @post.project
+      
+      # Check if user can view private posts
+      unless @post.public || (current_user && can_update_resource?(@project))
+        redirect_to project_posts_path(@project), alert: 'You do not have permission to view this private post.'
+        return
+      end
     end
 
     def edit
@@ -50,7 +62,7 @@ module Projects
     end
 
     def post_params
-      params.require(:post).permit(:title, :content, :featured_image, :short_description)
+      params.require(:post).permit(:title, :content, :featured_image, :short_description, :public)
     end
   end
 end
